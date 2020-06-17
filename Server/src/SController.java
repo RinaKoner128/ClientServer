d84d591c2167c29e11037f275bcaa1feee304ced
RinaKoner128 <54884833+RinaKoner128@users.noreply.github.com>
2020-06-17 12:05:06 +0300

    Поправки

--- Server/src/SController.java
+++ Server/src/SController.java
@@ -1,14 +1,18 @@
 import ClientServers.ClServ;
+import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
 
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 public class SController implements Initializable {
     public String request;
     public String respons;
+    public Map<String, Integer> map = new HashMap();
     private String res;
 
     @Override
@@ -20,19 +24,34 @@ public class SController implements Initializable {
             while (true)
                 try (ClServ module = new ClServ(server)) {
                     request = module.readerLine();
+
                     synchronized (this) {
                         if (request.equals("1")) {
                             module.writeLine(res);
                         } else {
-                            System.out.println("" + request);
-                            respons = (int) (Math.random() * 30 - 10) + "°";
-                            module.writeLine(request);
-                            module.writeLine(respons);
-                            System.out.println("" + respons);
-                            res = new String(request + respons);
+
+                                if (map != null && map.equals(request) ) {
+                                    respons = map.get(request).toString();
+                                    module.writeLine(request);
+                                    module.writeLine(respons);
+                                    System.out.println("" + request);
+                                    System.out.println("" + respons);
+                                    res = new String(request + respons);
+                                    System.out.println(res);
+                                } else {
+
+                                    respons = module.readerLine();
+                                    map.put(request, Integer.valueOf(respons));
+                                    module.writeLine(request);
+                                    module.writeLine(respons);
+                                    System.out.println(map);
+                                    //System.out.println("" + request);
+                                    //System.out.println("" + respons);
+                                    res = new String(request + respons);
+                                    System.out.println(res);
+                                }
                         }
                     }
-
 
                 } catch (IOException e) {
                     throw new RuntimeException(e);
